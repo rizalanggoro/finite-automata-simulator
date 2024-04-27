3a949996b846f18dc0f9c4f811a07193ceee9531
Rizal Anggoro <gnoogler4@gmail.com>
2024-04-27 15:36:05 +0700

    refactor: update copy diagram code help

--- src/components/mermaid.tsx
+++ src/components/mermaid.tsx
@@ -1,4 +1,4 @@
-import { Clipboard } from "lucide-react";
+import { CircleHelp, Clipboard, ClipboardCheck } from "lucide-react";
 import mermaid from "mermaid";
 import Link from "next/link";
 import { useEffect } from "react";
@@ -41,19 +41,31 @@ export default function MermaidComponent({
         {chart}
       </div>
 
-      <div className="flex justify-end items-center gap-4">
-        <p className="text-sm">
-          State diagram tidak terlihat dengan jelas? Salin kode state diagram
-          kemudian tempelkan pada website{" "}
-          <Button asChild variant={"link"} className="px-0">
-            <Link href={"https://mermaid.live"} target="_blank">
-              mermaid.live
-            </Link>
-          </Button>
-        </p>
-        <Button onClick={setCopied}>
-          <Clipboard className="w-4 h-4 mr-2" />
-          Salin kode
+      <div className="flex items-start gap-4 border rounded-lg bg-card p-4">
+        <CircleHelp className="w-4 h-4 my-1" />
+
+        <div className="flex-1">
+          <p className="font-semibold">
+            State diagram tidak terlihat dengan jelas?
+          </p>
+          <p className="text-sm">
+            Salin kode state diagram dengan menekan tombol di samping, kemudian
+            tempelkan pada website{" "}
+            <Button asChild variant={"link"} className="p-0 h-fit">
+              <Link href={"https://mermaid.live"} target="_blank">
+                mermaid.live
+              </Link>
+            </Button>
+            .
+          </p>
+        </div>
+
+        <Button onClick={setCopied} size={"icon"} variant={"secondary"}>
+          {isCopied ? (
+            <ClipboardCheck className="w-4 h-4" />
+          ) : (
+            <Clipboard className="w-4 h-4" />
+          )}
         </Button>
       </div>
     </div>
